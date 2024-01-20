0659a4e8e4dc904d034044f3202b53193bca96e7
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-20 08:35:46 -0800

    commit it

--- myapp/src/componnts/Navbar.jsx
+++ myapp/src/componnts/Navbar.jsx
@@ -1,10 +1,23 @@
 
 import React from "react";
-
+import logo from"../assets/images/logo.png"
 const Navbar = ()=>{
     return(
         <>
-        
+        <header className="header">
+            <a href="#" className="logo"></a>
+            <img src={logo} alt="" srcset="" />
+
+            <nav className="navbar">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#menu">Menu</a>
+                <a href="#products">Products</a>
+                <a href="#review">Review</a>
+                <a href="#contact">Contact</a>
+                <a href="#blog">Blogs</a>
+            </nav>
+        </header>
         </>
     );
 }
