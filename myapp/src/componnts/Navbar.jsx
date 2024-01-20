f253e8816af8217bd81b529709a0e49e837bdae3
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-20 09:19:33 -0800

    commit it

--- myapp/src/componnts/Navbar.jsx
+++ myapp/src/componnts/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useRef } from "react";
 import logo from "../assets/images/logo.png";
 const Navbar = () => {
-    const searchRef= useRef()
+    const searchRef= useRef();
     const searchHandle=()=>{
         searchRef.current.classList.toggle("active");
     }
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
       <header className="header">
         <a href="#" className="logo"></a>
-        <img src={logo} alt="" srcset="" />
+        <img src={logo} alt=""  />
 
         <nav className="navbar">
           <a href="#home">Home</a>
@@ -25,13 +25,13 @@ const Navbar = () => {
         </nav>
 
         <div className="icons">
-          <div className="fas fa-search">
+          <div className="fas fa-search" onClick={searchHandle}>
             <div className="fas fa-shopping-cart"></div>
             <div className="fas fa-bars" id="menu-btn"></div>
           </div>
         </div>
         <div  className="search-form"  ref={searchRef}>
-            <input type="search" name="" id="search-box" placeholder="Search Here..." onClick={searchHandle} />
+            <input type="search" name="" id="search-box" placeholder="Search Here..."  />
             <label htmlFor="search-box" className="fas fa-search"></label>
         </div>
       </header>
