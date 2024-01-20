9d1ced3ba821bccfe44ca0a38cdcd46e69627b98
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-20 08:54:47 -0800

    Update Navbar.jsx

--- myapp/src/componnts/Navbar.jsx
+++ myapp/src/componnts/Navbar.jsx
@@ -1,32 +1,34 @@
-
 import React from "react";
-import logo from"../assets/images/logo.png"
-const Navbar = ()=>{
-    return(
-        <>
-        <header className="header">
-            <a href="#" className="logo"></a>
-            <img src={logo} alt="" srcset="" />
+import logo from "../assets/images/logo.png";
+const Navbar = () => {
+  return (
+    <>
+      <header className="header">
+        <a href="#" className="logo"></a>
+        <img src={logo} alt="" srcset="" />
 
-            <nav className="navbar">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#menu">Menu</a>
-                <a href="#products">Products</a>
-                <a href="#review">Review</a>
-                <a href="#contact">Contact</a>
-                <a href="#blog">Blogs</a>
-            </nav>
+        <nav className="navbar">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#menu">Menu</a>
+          <a href="#products">Products</a>
+          <a href="#review">Review</a>
+          <a href="#contact">Contact</a>
+          <a href="#blog">Blogs</a>
+        </nav>
 
-            <div className="icons">
-                <div className="fas fa-search">
-                    <div className="fas fa-shopping-cart"></div>
-                    <div className="fas fa-bars" id="menu-btn"></div>
-                </div>
-            </div>
-        </header>
-        </>
-    );
-}
+        <div className="icons">
+          <div className="fas fa-search">
+            <div className="fas fa-shopping-cart"></div>
+            <div className="fas fa-bars" id="menu-btn"></div>
+          </div>
+        </div>
+        <div>
+            <input type="search" name="" id="" placeholder="Search Here"/>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
