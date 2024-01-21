2084d20d53ae2df3d43012c6c1999c1fd98866de
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-21 09:42:41 -0800

    commit it

--- myapp/src/componnts/Home.jsx
+++ myapp/src/componnts/Home.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-
-
-const Home=()=>
-{
-    return(
-        <>
-        
-        <section className="home" id="home">
-            <div className="content">
-                <h3>fresh <span>food in the forning</span></h3>
-            </div>
-        </section>
-        
-        </>
-    );
-}
+const Home = () => {
+  return (
+    <>
+      <section className="home" id="home">
+        <div className="content">
+          <h3>
+            fresh <span>food in the forning</span>
+          </h3>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto odio
+          possimus assumenda. Nemo facilis deserunt, illum consectetur fugit hic
+          possimus placeat porro doloribus nihil earum commodi sequi aliquam
+          velit rerum!
+        </p>
+      </section>
+    </>
+  );
+};
 export default Home;
