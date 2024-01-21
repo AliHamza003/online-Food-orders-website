f6d662ec06b8dfddefeb7f131655a307be24f3c1
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-21 09:36:18 -0800

    commit it

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Navbar from "./componnts/Navbar";
 import "../src/assets/css/style.css"
+import {Home} from "./componnts/Home";
 const App = ()=>{
   return(
     <>
--- myapp/src/componnts/Home.jsx
+++ myapp/src/componnts/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "./assets/css/style.css";
-import Navbar from "./Navbar";
-import Home from "./compoenets/Home"
+
+
 
 const App=()=>
 {
     return(
         <>
-        <Navbar/>
-        <Home/>
+        
+        
         
         </>
     );
