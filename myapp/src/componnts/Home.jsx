2350e09546e0566a2011999f28e1798ed58e286a
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-21 09:38:24 -0800

    Update Home.jsx

--- myapp/src/componnts/Home.jsx
+++ myapp/src/componnts/Home.jsx
@@ -8,7 +8,9 @@ const App=()=>
         <>
         
         <section className="home" id="home">
-            <div className="content"></div>
+            <div className="content">
+                <h3>Fresh <span>Food in the Morning</span></h3>
+            </div>
         </section>
         
         </>
