5ae90768fac3677d7d3b60da695585df9c422fde
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-21 09:32:40 -0800

    Update Home.jsx

--- myapp/src/componnts/Home.jsx
+++ myapp/src/componnts/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./assets/css/style.css";
 import Navbar from "./Navbar";
+import Home from "./compoenets/Home"
 
 const App=()=>
 {
     return(
         <>
         <Navbar/>
+        
         </>
     );
 }
