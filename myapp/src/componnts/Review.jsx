2a55a5199a807e8d52e8abdaf0d446fa775895dc
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:44:31 -0800

    Update Review.jsx

--- myapp/src/componnts/Review.jsx
+++ myapp/src/componnts/Review.jsx
@@ -3,6 +3,9 @@ import { review } from "../Data";
 const Review=()=>{
     return(
         <>
+        <section >
+            <h1 className="heading"></h1>
+        </section>
         </>
     );
 };
