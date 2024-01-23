d0983cce664076c2933761687b30dc1dbe1ae34d
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:14:01 -0800

    Update Products.jsx

--- myapp/src/componnts/Products.jsx
+++ myapp/src/componnts/Products.jsx
@@ -4,7 +4,7 @@ const Products=()=>{
         <>
         <section className="products" id="products">
             <h1 className="heading">
-                
+                our <span>products</span>
             </h1>
         </section>
 
