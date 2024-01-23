f13919e85741836c443d0f3ca879283e32c03996
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:51:04 -0800

    c

--- myapp/src/componnts/Review.jsx
+++ myapp/src/componnts/Review.jsx
@@ -12,8 +12,13 @@ const Review=()=>{
                 {
                     review.map((item, index)=>(
                         <div className="box">
-                            <img src={qoutImg} alt=""  />
+                            <img src={qoutImg} alt=""  className="qoute" />
                         </div>
+
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut amet nisi dignissimos, non velit reprehenderit qui tempora, quo rerum exercitationem ipsam molestiae cum! Neque, beatae! Provident aperiam facere necessitatibus voluptatibus.
+                        </p>
+
                     ))
                 }
             </div>
