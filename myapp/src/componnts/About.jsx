7e13a8b4299dd31d35f7f4d8212a384d5a895d7d
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:14:49 -0800

    Update About.jsx

--- myapp/src/componnts/About.jsx
+++ myapp/src/componnts/About.jsx
@@ -7,10 +7,17 @@ const About = () => {
       </h1>
       <div className="row">
         <div className="img">
-          <img src={aboutimg} alt="" srcset="" />
+          <img src={aboutimg} alt="" />
         </div>
         <div className="content">
-            <h3>what makes our food special</h3>
+          <h3>what makes our food special?</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+            corporis nesciunt eligendi laborum alias odio officiis, ipsam
+            veritatis
+            animi eveniet consequatur totam esse officia blanditiis
+            qui saepe corrupti assumenda repellat?
+          </p>
         </div>
       </div>
     </section>
