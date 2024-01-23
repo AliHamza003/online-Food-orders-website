48bdd957921e3f2c09ee3d5a26dfb362b6faec4b
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:05:40 -0800

    Update About.jsx

--- myapp/src/componnts/About.jsx
+++ myapp/src/componnts/About.jsx
@@ -1,11 +1,16 @@
 
-
+import about-img from "../assets/images/about-img.jpeg"
 const About=()=>{
     return(
         <section className="about" id="about">
             <h1 className="heading">
                 <span>about</span> us
             </h1>
+            <div className="row">
+                <div className="img">
+                    <img src={about-img} alt="" srcset="" />
+                </div>
+            </div>
         </section>
     );
 }
