a7757019bfd884aa10c46c20f220677d74fb0ec2
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:52:47 -0800

    Update Review.jsx

--- myapp/src/componnts/Review.jsx
+++ myapp/src/componnts/Review.jsx
@@ -1,32 +1,32 @@
 import { review } from "../Data";
-import qoutImg from "../assets/images/quote-img.png"
+import qoutImg from "../assets/images/quote-img.png";
 
-const Review=()=>{
-    return(
-        <>
-        <section >
-            <h1 className="heading">
-                sustomers's <span>review</span>
-            </h1>
-            <div className="box-container">
-                {
-                    review.map((item, index)=>(
-                        <div className="box">
-                            <img src={qoutImg} alt=""  className="qoute" />
+const Review = () => {
+  return (
+    <>
+      <section>
+        <h1 className="heading">
+          sustomers's <span>review</span>
+        </h1>
+        <div className="box-container">
+          {review.map((item, index) => (
+            <div className="box">
+              <img src={qoutImg} alt="" className="qoute" />
 
-                            <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut amet nisi dignissimos, non velit reprehenderit qui tempora, quo rerum exercitationem ipsam molestiae cum! Neque, beatae! Provident aperiam facere necessitatibus voluptatibus.
-                        </p>
-                        </div>
-
-                       
-
-                    ))
-                }
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
+                amet
+                nisi dignissimos, non velit reprehenderit qui tempora, quo
+                rerum exercitationem ipsam molestiae cum! Neque,
+                beatae!
+                Provident aperiam?
+              </p>
             </div>
-        </section>
-        </>
-    );
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Review;
