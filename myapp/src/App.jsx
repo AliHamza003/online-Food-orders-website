67173a922a697641ecb3c9ad9cf33fa4673ce876
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:23:19 -0800

    commit it

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -5,12 +5,14 @@ import Navbar from "./componnts/Navbar";
 import "../src/assets/css/style.css"
 import Home from "./componnts/Home";
 import About from "./componnts/About"
+import Menu from "./componnts/Menu";
 const App = ()=>{
   return(
     <>
     <Navbar/>
     <Home/>
     <About/>
+    <Menu/>
     </>
   )
 }
