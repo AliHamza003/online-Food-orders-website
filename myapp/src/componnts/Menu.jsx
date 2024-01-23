0626a890008e7c3495ffb35986b5006f9f1bb3cb
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:26:01 -0800

    Update Menu.jsx

--- myapp/src/componnts/Menu.jsx
+++ myapp/src/componnts/Menu.jsx
@@ -1,7 +1,9 @@
 
 const Menu =()=>{
     return(
-        
+        <section className="menu" id="menu">
+            <h1 className="heading">our <span>menu</span></h1>
+        </section>
     );
 }
 export default Menu
