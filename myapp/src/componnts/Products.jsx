da37c5cc41290d8779e20a9fe3d30f9ccb7193fb
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:14:52 -0800

    Update Products.jsx

--- myapp/src/componnts/Products.jsx
+++ myapp/src/componnts/Products.jsx
@@ -1,15 +1,13 @@
-const Products=()=>{
-    return(
-
-        <>
-        <section className="products" id="products">
-            <h1 className="heading">
-                our <span>products</span>
-            </h1>
-        </section>
-
-        </>
-    );
-
-}
+const Products = () => {
+  return (
+    <>
+      <section className="products" id="products">
+        <h1 className="heading">
+          our <span>products</span>
+        </h1>
+        <div className="box-container"></div>
+      </section>
+    </>
+  );
+};
 export default Products;
