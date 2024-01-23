99c225bcd37cbe587f46cbe9332dd222df77bf81
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:13:24 -0800

    Update About.jsx

--- myapp/src/componnts/About.jsx
+++ myapp/src/componnts/About.jsx
@@ -1,18 +1,20 @@
-
-import aboutimg from "../assets/images/about-img.jpg"
-const About=()=>{
-    return(
-        <section className="about" id="about">
-            <h1 className="heading">
-                <span>about</span> us
-            </h1>
-            <div className="row">
-                <div className="img">
-                    <img src={aboutimg} alt="" srcset="" />
-                </div>
-            </div>
-        </section>
-    );
-}
+import aboutimg from "../assets/images/about-img.jpg";
+const About = () => {
+  return (
+    <section className="about" id="about">
+      <h1 className="heading">
+        <span>about</span> us
+      </h1>
+      <div className="row">
+        <div className="img">
+          <img src={aboutimg} alt="" srcset="" />
+        </div>
+        <div className="content">
+            <h3>what makes our food special</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
