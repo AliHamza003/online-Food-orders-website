902e9b8c8a5b3b0a91a2399849f561ad7af03e91
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:01:18 -0800

    Update About.jsx

--- myapp/src/componnts/About.jsx
+++ myapp/src/componnts/About.jsx
@@ -3,7 +3,9 @@
 const About=()=>{
     return(
         <section className="about" id="about">
-            
+            <h1 className="heading">
+                <span>about</span>
+            </h1>
         </section>
     );
 }
