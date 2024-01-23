f4543d11505de15d98788fbc38e8101fd9f03db6
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 22:29:46 -0800

    Update Menu.jsx

--- myapp/src/componnts/Menu.jsx
+++ myapp/src/componnts/Menu.jsx
@@ -1,8 +1,17 @@
-
+import  menu  from "../Data";
 const Menu =()=>{
     return(
         <section className="menu" id="menu">
             <h1 className="heading">our <span>menu</span></h1>
+            <div className="box-container">
+                {
+                    menu.map((item, index)=>{
+                        <div className="">
+                            <img src={item.img} alt="" />
+                        </div>
+                    })
+                }
+            </div>
         </section>
     );
 }
