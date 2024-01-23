ac95c60b6639b9d06903e57fa99c9de7c96642c8
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-22 23:29:24 -0800

    Update Products.jsx

--- myapp/src/componnts/Products.jsx
+++ myapp/src/componnts/Products.jsx
@@ -1,4 +1,4 @@
-import {product} from "../Data"
+import { product } from "../Data";
 const Products = () => {
   return (
     <>
@@ -7,28 +7,28 @@ const Products = () => {
           our <span>products</span>
         </h1>
         <div className="box-container">
-            {product.map((item, index)=>(
-                    <div className="box">
-                        <div className="icons">
-                            <a href="#" className="fas fa-shoping-cart"></a>
-                            <a href="#" className="fas fa-heart"></a>
-                            <a href="#" className="fas fa-eye"></a>
-                        </div>
-                        <div className="image">
-                            <img src={item.img} alt="" srcset="" />
-                        </div>
-                        <div className="content">
-                            <h3>fresh coffee</h3>
-                            <div className="stars">
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            
+          {product.map((item, index) => (
+            <div className="box">
+              <div className="icons">
+                <a href="#" className="fas fa-shoping-cart"></a>
+                <a href="#" className="fas fa-heart"></a>
+                <a href="#" className="fas fa-eye"></a>
+              </div>
+              <div className="image">
+                <img src={item.img} alt="" srcset="" />
+              </div>
+              <div className="content">
+                <h3>fresh coffee</h3>
+                <div className="stars">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star-half-alt"></i>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </>
