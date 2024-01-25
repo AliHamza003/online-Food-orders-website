ea7162d43477f66b132e3667241063255fd56b6d
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-24 21:18:00 -0800

    Update contact.jsx

--- myapp/src/componnts/contact.jsx
+++ myapp/src/componnts/contact.jsx
@@ -18,21 +18,21 @@ const Contact = () => {
             </h3>
             <div className="inputBox">
                 <span className="fas fa-user">
-                    <input type="text" placeholder="name" />
+                    
                 </span>
+                <input type="text" placeholder="name" />
             </div>
 
             <div className="inputBox">
-                <span className="fas fa-envelope">
-                    <input type="email" placeholder="email" />
-                </span>
+                <span className="fas fa-envelope"></span>
+                <input type="email" placeholder="email" />
             </div>
 
              <div className="inputBox">
-                <span className="fas fa-phone">
-                    <input type="number" placeholder="number" />
-                </span>
+                <span className="fas fa-phone"></span>
+                <input type="number" placeholder="number" />
             </div>
+            <input type="submit" value="contact now" className="btn" />
           </form>
         </div>
       </section>
