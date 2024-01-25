856d55956941ec639bbab5835823be167f53b083
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-24 21:07:11 -0800

    Update contact.jsx

--- myapp/src/componnts/contact.jsx
+++ myapp/src/componnts/contact.jsx
@@ -3,8 +3,16 @@ const Contact = () => {
     <>
       <section className="contact" id="contact">
         <h1 className="heading">
-            <span>Contact</span> US
+          <span>Contact</span> US
         </h1>
+        <div className="row">
+          <iframe
+            class="map"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11993.267641772954!2d-72.8480109!3d41.2802068!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x36c6fa619c4f5603!2sMcDonald&#39;s!5e0!3m2!1sen!2s!4v1633364807635!5m2!1sen!2s"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
       </section>
     </>
   );
