583ab9bc191dcd10eb6a17c57c51cb8e852a3997
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-24 21:05:01 -0800

    Update contact.jsx

--- myapp/src/componnts/contact.jsx
+++ myapp/src/componnts/contact.jsx
@@ -1,12 +1,13 @@
-const Contact =()=>{
-    return(
-        <>
-        <section className="contact" id="contact">
-
-</section>
-        </>
-        
-    );
-}
+const Contact = () => {
+  return (
+    <>
+      <section className="contact" id="contact">
+        <h1 className="heading">
+            <span>Contact</span> US
+        </h1>
+      </section>
+    </>
+  );
+};
 
 export default Contact;
